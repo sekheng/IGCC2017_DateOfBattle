8bccc76fc50ae93be9fdaf02c07f758d8975c06f
Ah Heng <leesekheng@yahoo.com>
2017-09-09 23:12:19 +0800

    In progress of creating game rules.

---
+++ DateOfBattle/Assets/Script/Game Manager/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This will handle the entire game flow.
+/// Unfortunately, no time to code proper game rules. LOL.
+/// </summary>
+public class GameManager : MonoBehaviour {
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnEnable()
+    {
+        ObserverSystemScript.Instance.SubscribeEvent("PlayerFortressLost", playerLostFortress);
+        ObserverSystemScript.Instance.SubscribeEvent("EnemyFortressLost", enemyLostFortress);
+    }
+
+    private void OnDisable()
+    {
+        ObserverSystemScript.Instance.UnsubscribeEvent("PlayerFortressLost", playerLostFortress);
+        ObserverSystemScript.Instance.UnsubscribeEvent("EnemyFortressLost", enemyLostFortress);
+    }
+
+    protected void playerLostFortress()
+    {
+
+    }
+
+    protected void enemyLostFortress()
+    {
+
+    }
+}
--- DateOfBattle/Assets/Script/Sek Heng Test Script/testStateChange.cs
+++ DateOfBattle/Assets/Script/Sek Heng Test Script/testStateChange.cs
@@ -17,6 +17,7 @@ public class testStateChange : MonoBehaviour {
 	
     IEnumerator changeStateAfterStart()
     {
+        // Wait for 2 frames then start to change state because the state machine and the states has yet to be initialised!
         yield return null;
         yield return null;
         unitStateMachine.GetGenericState("MoveState").interactWithState(targetTile);
