6071322db69cb7333a6c620ff27148d0ebe79d08
TSITCL\s152100 <sasamoto2016@gmail.com>
2017-09-13 14:15:38 +0900

    AudioManager

---
+++ DateOfBattle/Assets/Script/Aki test/AudioManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioSource seSource;
+    public AudioSource bgmSource;
+    public AudioClip[] se;
+    public AudioClip[] bgm;
+    // Use this for initialization
+    void Start () {
+
+    }
+    public bool PlayMusic(int clipNum)
+    {
+        if(clipNum > bgm.Length)
+        {
+            return false;
+        }
+        bgmSource.clip = bgm[clipNum];
+        bgmSource.Play();
+        return true;
+    }
+
+    public bool PlaySE(int clipNum)
+    {
+        if (clipNum > se.Length)
+        {
+            return false;
+        }
+        seSource.PlayOneShot(se[clipNum]);
+        return true;
+    }
+    public bool bgmLoop
+    {
+        set
+        {
+            bgmSource.loop = value;
+        }
+        get
+        {
+            return bgmSource.loop;
+        }
+    }
+    public float bgmVolume
+    {
+        set
+        {
+            bgmSource.volume = value;
+        }
+        get
+        {
+            return bgmSource.volume;
+        }
+    }
+    public float seVolume
+    {
+        set
+        {
+            seSource.volume = value;
+        }
+        get
+        {
+            return seSource.volume;
+        }
+    }
+
+}
---
+++ DateOfBattle/Assets/soundSourceTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundSourceTest : MonoBehaviour {
+
+    AudioManager manager;
+	// Use this for initialization
+	void Start () {
+        manager = GetComponent<AudioManager>();
+
+    }
+
+    //example,don't use this.
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            manager.PlaySE(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            manager.PlaySE(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            manager.PlaySE(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            manager.PlayMusic(0);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            manager.PlayMusic(1);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            manager.PlayMusic(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            manager.bgmLoop = manager.bgmLoop;
+        }
+    }
+
+}
